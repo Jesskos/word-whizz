7b71e2536a1c02b21fc88a271b209288577f908e
Jessica Koscheka <jesskos@gmail.com>
2018-11-17 11:24:55 -0800

    added render_template to server

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "hello world"
+	return render_template('index.html')
 
 @app.route('/play')
 def play_game():
-	pass
+	return render_template("game.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
