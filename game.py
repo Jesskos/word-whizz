33cd5640f47347665600fa7ef31b30d345ec838c
Jessica Koscheka <jesskos@gmail.com>
2018-11-17 13:14:38 -0800

    added initial game logic

--- game.py
+++ game.py
@@ -1,5 +1,48 @@
+word = "chocolate"
+correct_guessed_letters = set()
+incorrect_guessed_letters = set()
+incorrect_guesses = 0 
+max_incorrect_guesses = 6 
 
 def get_word():
 	''' returns a word to guess'''
 
-	return "chocolate"
+	return word
+
+def get_word_length():
+	''' gets the length of the word'''
+
+	return len(word)
+
+def check_letter(letter):
+	''' checks if the letter is in the word and adjusts the global lists and counters based on the response'''
+
+	print("word is {} letter is {}".format(word, letter))
+	global incorrect_guesses
+	if letter in word and (letter not in incorrect_guessed_letters or letter not in correct_guessed_letters):
+		correct_guessed_letters.add(letter)
+		return True
+	else:
+		incorrect_guessed_letters.add(letter)
+		incorrect_guesses += 1 
+		return False
+
+def is_already_guessed_letter(letter):
+	''' checks whether or not the letter has been previously gussed by the user'''
+
+	if letter in incorrect_guessed_letters or letter in correct_guessed_letters:
+		return True 
+	else:
+		return False
+
+def guesses_left():
+	''' returns the remaining guesses''' 
+	remaining_guesses = max_incorrect_guesses - incorrect_guesses
+	return remaining_guesses
+
+def game_over():
+	''' ends the game when the user exceeds allotted guesses'''
+	if remaining_guesses == 0:
+		return True
+	else:
+		return False
--- server.py
+++ server.py
@@ -11,7 +11,27 @@ def index():
 @app.route('/play')
 def play_game():
 	word = get_word()
-	return render_template("game.html", word = word)
+	length_word = get_word_length()
+	return render_template("game.html", length = length_word)
+
+
+@app.route('/check')
+def check():
+	letter = request.args.get('letter-input') 
+	print("{} is the letter".format(letter))
+	if is_already_guessed_letter(letter):
+		return "You already guessed this letter"
+	else:
+		checked_letter = check_letter(letter)
+		guesses_remaining = guesses_left()
+		if checked_letter:
+			return "Great Work! Correct Guess!"
+		else:
+			return "Sorry, Incorrect Guess! You have {} chances remaining".format(guesses_remaining)
+
+
+
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
