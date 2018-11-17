15b8f6e656bf3de37e7664e849892274400ba870
Jessica Koscheka <jesskos@gmail.com>
2018-11-17 11:38:44 -0800

    imported game.py and added to logic

--- server.py
+++ server.py
@@ -1,14 +1,17 @@
 from flask import Flask, request, render_template
+from game import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+
 	return render_template('index.html')
 
 @app.route('/play')
 def play_game():
-	return render_template("game.html")
+	word = get_word()
+	return render_template("game.html", word = word)
 
 if __name__ == "__main__":
 	app.run(debug=True)
