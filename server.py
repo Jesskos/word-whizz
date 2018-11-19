253b4d32324f54de678dc07654f0208d59784c55
Jessica Koscheka <jesskos@gmail.com>
2018-11-19 12:22:29 -0800

    added documentation to server.py and game.js and organized python code to return one JSON reply

--- server.py
+++ server.py
@@ -22,32 +22,56 @@ def play_game():
 
 @app.route('/check', methods=['GET'])
 def check():
-	''' carries out came logic based on instance of Game() class '''
+	''' carries out came logic based on instance of Game() class, and responds with appropriate message and information '''
 
+	game_response = {}
+
+	#checks to make sure the game has not already ended
 	if word_game.game_over():
-		return jsonify({"message":"The Game is over. Please choose to play again"})
-	letter = request.args.get('letter') 
-	if word_game.is_already_guessed_letter(letter):
-		return jsonify({"message": "You already guessed the letter {}".format(letter)})
+		game_response["message"] = "The Game is over. Please choose to play again"
+	# if not game over, carries out game logic
 	else:
-		checked_letter = word_game.check_letter(letter)
-		remaining_guesses = word_game.guesses_left()
-		indices_of_letter_in_word = word_game.get_indices_of_letter_in_word(letter)
-		if checked_letter:
-			if word_game.win():
-				return jsonify({"message": "You win!", 
-					"indices": indices_of_letter_in_word})
-			return jsonify({"message": "Great Work! Correct Guess!", 
-				"indices": indices_of_letter_in_word})
-		else:
-			if word_game.lose():
-				return jsonify({"message":"Sorry, you have lost the game.", 
-					"remaining_guesses": remaining_guesses, 
-					"word": word_game.get_word()})
+		# receives the chosen letter 
+		letter = request.args.get('letter') 
 
-			return jsonify({"message": "Sorry, Incorrect Guess! {} is not in the word. You have {} chances remaining".
-				format(letter, remaining_guesses), 
-				"remaining_guesses": remaining_guesses})
+		# checks if the letter in the word has already been guessed.
+		if word_game.is_already_guessed_letter(letter):
+			game_response["message"] = "You already guessed the letter {}".format(letter)
+
+		# if the letter has not been gussed yet, checks whether or not the letter is in the word
+		else:
+			checked_letter = word_game.check_letter(letter)
+			remaining_guesses = word_game.guesses_left()
+			indices_of_letter_in_word = word_game.get_indices_of_letter_in_word(letter)
+
+			# if the checked letter is in the word, will include the indices of the checked letter in the word in the response.
+			# will also check if the player has completed the word and won when they make the guess
+			if checked_letter:
+				game_response['indices'] = indices_of_letter_in_word
+
+				#player has won the game after guessing letter
+				if word_game.win():
+					game_response['message'] = "You win!"
+				
+				#player correctly guessed letter, but has not yet won
+				else:
+					game_response["message"] = "Great Work! Correct Guess!"
+
+			# if the checked letter is not in the word, will include the number of remaing guesses in the response
+			# will also check if the player has lost the game with the wrong guess, and if so, include the whole word in the response
+			else:
+				game_response["remaining_guesses"]=remaining_guesses
+
+				# the player guessed an incorrect letter, and lost the game after exceeding incorrect guesses
+				if word_game.lose():
+					game_response["message"] = "Sorry, you have lost the game."
+					game_response["word"] = word_game.get_word()
+
+				# the player guessed an incorrect letter, but still has more tries
+				else: 
+					game_response["message"] = "Sorry, Incorrect Guess! {} is not in the word. You have {} chances remaining".format(letter, remaining_guesses)
+
+	return jsonify(game_response)
 
 
 
--- static/js/game.js
+++ static/js/game.js
@@ -8,15 +8,23 @@ function checkLetter(evt) {
 	};
 
 	$.get('/check', letterChoice, (results) => {
-		if (results.hasOwnProperty('indices')) {
-			let indices = results['indices'];
+
+		// checks for indices in the response, sent only when letter is correct, corresponding to element id
+		if (results.hasOwnProperty("indices")) {
+			let indices = results["indices"];
 			addLetter(indices, letterChoice['letter']);
+
+		// checks for "Incorrect Guess" in the response, sent only when incorrect guess made, to add to list of incorrect letters in html
 		} else if (results["message"].includes("Sorry, Incorrect Guess!")) {
 			$("#incorrect-letters-guessed").append(letterChoice["letter"] + " ");
-		} else if (results.hasOwnProperty('word')) {
+		
+		// checks for "word" in the response in case player has lost, and need to show resulting word
+		} else if (results.hasOwnProperty("word")) {
 			$("#incorrect-letters-guessed-list").append(letterChoice["letter" + " "])
 			$("div#word-to-guess").html(results["word"])
 		};
+
+		// show how many guesses remain, and alert with message after each guess
 		$("#num-remaining-guesses").html(results["remaining_guesses"])
 		alert(results["message"]);
 
@@ -24,7 +32,8 @@ function checkLetter(evt) {
 }
 
 function addLetter(indices, letter) {
-	console.log("inside add letters")
+	// a functin to add letters to html by searing for element id
+
 	let i;
 	for (i=0; i<indices.length; i++){
 		console.log(i)
@@ -34,4 +43,5 @@ function addLetter(indices, letter) {
 	};
 }
 
+// check letter every time form is submitted
 $("#letter-guessing-form").on('submit', checkLetter)
