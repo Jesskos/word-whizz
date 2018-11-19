b59c3c20e0bb2b261a6debe64c1876b5bb08d6fa
Jessica Koscheka <jesskos@gmail.com>
2018-11-18 19:37:26 -0800

    corrected a couple of syntax errors

--- model.py
+++ model.py
@@ -2,7 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model)
+class User(db.Model):
 	''' A model of users and their scores '''
 
 	__tablename__ = "users"
@@ -18,7 +18,7 @@ class User(db.Model)
 		return "<User user_id={}  username={} password={} total_score={}>".format(self.user_id, 
 			self.username, self.password, self.total_score) # pragma: no cover
 
-class Score(db.Model)
+class Score(db.Model):
 	''' keeps track of the score from each game'''
 	__tablename__ = "scores"
 
@@ -26,13 +26,15 @@ class Score(db.Model)
 	date = db.Column(db.DateTime, nullable=False)
 	word = db.Column(db.String(20), nullable=False)
 	score = db.Column(db.Integer, nullable=False)
-	finished = db.Column(db.Integer, nullable=False
+	finished = db.Column(db.Integer, nullable=False)
 
 	def __repr__(self):
 		""" Provide helpful representation of score object when printed"""
 
 		return "<Score date={}  word={} word={} score={} finished={}>".format(self.score_id, 
 			self.date, self.word, self.score, self.finished) # pragma: no cover
+
+
 
 
 def connect_to_db(app, uri='postgresql:///scoresdb'): # pragma: no cover
@@ -50,4 +52,4 @@ if __name__ == "__main__":
 
 	from server import app  # pragma: no cover
 	connect_to_db(app)  # pragma: no cover
-	print "Connected to DB."  # pragma: no cover
+	print("Connected to DB.")  # pragma: no cover
