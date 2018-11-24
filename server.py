ca1c6777227740cee78a9ed7070edc3a8156046a
Jessica Koscheka <jesskos@gmail.com>
2018-11-24 11:45:51 -0800

    improved documentation of the server without changing any code

--- server.py
+++ server.py
@@ -4,11 +4,13 @@ import requests
 from model import connect_to_db, db, User, Score
 from datetime import datetime
 
-
+# create an instance of the flask application.__name__ tells Flask where to look for files, templates, etc.
 app = Flask(__name__)
 
-# instantiating a new game
+# instantiating a new game using the game class
 word_game = Game()
+
+# secret key for encrypting sessions
 app.secret_key="w()r|)gvu&&"
 
 @app.route('/')
@@ -17,41 +19,44 @@ def index():
 	if "user_id" in session:
 		return redirect("/play")
 
+	# renders index page
 	return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def log_in():
-	''' allows a user to login '''
+	''' allows a user to login to play the game by checking login credentials '''
 
-	# Receiving information from form to log user into the game. 
-	# request.form returns an immutable MultiDict(). Used this method to convert
+	# Receiving request from browser with form information
+	# with more time, I would spend more time on form validation to prevent security threats
+	# request.form returns an immutable MultiDict(). Used form.to_dict() to convert MultiDict() to dictionary
 	request_info = request.form.to_dict()
 	entered_username = request_info['InputUsername']
 	entered_password = request_info['InputPassword']
 	existing_user = User.query.filter(User.username==entered_username).first()
 
-	# checks to make sure user exists 
+	# checks to make sure user exists in database
 	if not existing_user:
 		flash("Username does not exist. Please sign up or check your spelling")
 		return redirect("/")
 
-	# if user exists, checks password, to make sure it matches database password
+	# if user exists, checks password, to make sure it matches password found in database 
 	# if so, logs user into the game. 
 	else:
 		if existing_user.password == entered_password:
 			session["user_id"] = existing_user.user_id
 			session["name"] = existing_user.username
 
-			#creates a session with a default difficulty level, and instantiates a new game
-			# in the future, the user will be able to choose a default difficulty level when registering/signing up and change it in his/her profile
+			#creates a session with a default difficulty level of '3', and instantiates a new game
+			# in the future, I would like the user to be able to choose a difficulty level when registering/signing up and change it in his/her profile
 			global word_game
 			session["difficulty_level"] = "3"
 			word_game = Game(session["difficulty_level"])
 
+			# when user logs in, flashes a message that they have successfully logged in (see html templating)
 			flash("you have successfully logged in")
 			return redirect("/play")
 
-		# if passwords do not match, user is redirected home
+		# if passwords do not match, user is redirected home, and flashed a message to try again
 		else:
 			flash("Password Incorrect. Please try again")
 			return redirect("/")
@@ -59,18 +64,20 @@ def log_in():
 
 @app.route('/signup', methods=['POST'])
 def sign_up():
-	''' allows a user to signup '''
+	''' allows a user to signup and adds new users to the database'''
 
-	# getting information from forms to sign up user to play game
+	# Receiving request from forms with username and password
+	# With more time, I would spend more time on form validation to prevent security risks
 	new_username = request.form["SignUpInputEmail"].lower()
 	new_password = request.form["SignUpPassword"]
 	confirm_password = request.form["ConfirmInputPassword"]
 
-	# checks if user is already existing user to prevent duplicate usernames
+	# checks if user is already an existing user to prevent duplicate usernames before adding name to database
 	existing_user = User.query.filter_by(username=new_username).first()
 
-	# checks if password matches confirmatory password
-	# In the future, I plan to use password encryption, validation, and set passwprd limits to increase security
+	# Checks if password matches confirmatory password
+	# If password matches confirmatory password and user is not an existing user, the new user is saved to the db so that they can log in.
+	# In the future, I plan to use password encryption, validation, and set password requirements to increase security
 	if new_password == confirm_password and not existing_user:
 		new_user = User(username = new_username, password=new_password)
 		db.session.add(new_user)
@@ -80,13 +87,14 @@ def sign_up():
 		session["user_id"] = new_user.user_id
 		session["name"] = new_user.username
 
-		#creates a session with a default difficulty level
-		# in the future, the user will be able to choose a default difficulty level when registering/signing up and change it in his/her profile
-		# A new game object is intantiated when user is confirmed and signs in
+		# Creates a session with a default difficulty level of '3'
+		# In the future, the user will be able to choose a difficulty level when registering/signing up and change it in their profile
+		# A new game object is instantiated when sign up process is completed
 		global word_game
 		session["difficulty_level"] = "3"
 		word_game = Game(session["difficulty_level"])
 
+		# User is forwarded to play game
 		return redirect("/play")
 
 	# checks to make sure passwords match
@@ -102,9 +110,9 @@ def sign_up():
 
 @app.route('/logout')
 def log_out():
-	''' logs a user out of the portal'''
+	''' logs a user out of the game, and ends session'''
 
-	# removes user_id from session, along with the word_game so it is not repeated for other users
+	# Removes user_id, name, and difficulty level from the session to log out user, and redirects back to index
 	if "user_id" in session:
 		del session["user_id"]
 		del session["name"]
@@ -115,19 +123,19 @@ def log_out():
 
 @app.route('/play')
 def play():
-	''' renders the game page '''
+	''' renders the game page using templating'''
 
 	# if user is not logged in, redirectsback to homepage
 	if "user_id" not in session:
 		return redirect("/")
 
-	#gets global variable wordgame
+	# gets global variable word_game
 	global word_game
 	word = word_game.get_word()
 	print(word)
 	
-	# gets length of word, incorrect_guessed_letters, length_of_word, and remaining_guesses for templating
-	# when page is refreshed, game will maintain where it left off
+	# Gets the length of word, incorrect_guessed_letters, length_of_word, and remaining_guesses using class methods or attributes
+	# When page is refreshed, game will pick up where it left off
 	incorrect_guessed_letters = word_game.incorrect_guessed_letters
 	correctly_guessed_letters = word_game.correct_guessed_letters
 	length_word = word_game.get_word_length()
@@ -135,9 +143,11 @@ def play():
 
 	print(length_word)
 
-	# makes a dictioary of the index as key, and the letter as values so that if the page is refreshed, letter location on board is maintained
+	# A dictionary to store correctly guessed letters and their indices
 	correctly_guessed_dictionary = {}
 
+	# Makes a dictioary with the index as key, and the letter as values so that if the page is refreshed, letter location on board is maintained
+	# If the game is lost, will go into else condition to make a dictionary with all letter indices to reveal entire word to user
 	if not word_game.lose():
 		for letter in correctly_guessed_letters:
 			indices = word_game.get_indices_of_letter_in_word(letter)
@@ -150,7 +160,7 @@ def play():
 
 	print("word is {} and difficulty_level is {}".format(word, word_game.difficulty_level))
 
-	# sends over variables to be used for Jinja templating
+	# Renders variables using Jinja2 templating
 	return render_template("game.html", length=length_word, guesses=remaining_guesses, 
 		incorrectly_guessed = incorrect_guessed_letters, correctly_guessed = correctly_guessed_dictionary, 
 		difficulty_level=session["difficulty_level"], name=session["name"])
@@ -160,24 +170,24 @@ def play():
 def play_again():
 	''' a route that responds to an AJAX call from the browser to refresh the word '''
 
-	# gets the global variable word_game 
+	# Gets the global variable word_game
 	global word_game
 
-	# checks if user changed the difficulty level
+	# Checks if user changed the difficulty level. Returns None if the user has not changed the difficutly level.
 	new_difficulty_level = request.args.get('difficulty-level')
 
-	# if the user changed the difficulty level, instantiates a new game with the new difficutly level as an argument
+	# If the user changed the difficulty level, instantiates a new game with the new difficutly level as an argument.
 	if new_difficulty_level:
 		session['difficulty_level'] = new_difficulty_level
 		word_game = Game(new_difficulty_level)
 
-	# if the user does not choose a new difficulty level, instantiates a new game with the current difficulty level from session as an argument
+	# If the user has not changed the difficulty level, instantiates a new game with the current difficulty level from session passed as an argument
 	else:
 		word_game = Game(session["difficulty_level"])
 
 	print("{} is the new difficulty_level and word is {} and difficulty is {}".format(new_difficulty_level, word_game.get_word(), session['difficulty_level']))
 
-	# gets the length of the word and remaining guesses to send to browser to modify DOM for new word
+	# Gets the length of the word and remaining guesses to send to browser to modify DOM for new word
 	length_word = word_game.get_word_length()
 	remaining_guesses = word_game.guesses_left()
 
@@ -186,68 +196,80 @@ def play_again():
 
 @app.route('/check', methods=['GET'])
 def check():
-	''' carries out came logic based on instance of Game() class, and responds with appropriate message and information '''
+	''' carries out came logic, and responds with appropriate message and information to AJAX calls from the browser '''
 
-	# an empty dictinary to be converted into JSON 
+	# An empty dictinary to be converted into JSON 
 	game_response = {}
 
-	#checks to make sure the game has not already ended
+	# Checks to make sure the game has not already ended
 	if word_game.game_over():
 		game_response["message"] = "The game is over. Please choose to play again"
-	# if not game over, carries out game logic
+
+	# If the game has not already ended, carries out game logic
 	else:
-		# receives the letter request from the browser
+		# Receives the request with the letter from the browser
+		# I plan to improve form validation in the future (making sure letter is one charater, only contains alpha characters, etc)
 		letter = request.args.get('letter') 
 		letter = letter.lower()
 
-		# checks if the letter in the word has already been guessed.
+		# Checks if the letter in the word has already been guessed.
 		if word_game.is_already_guessed_letter(letter):
 			game_response["message"] = "You already guessed the letter {}".format(letter)
 
-		# if the letter has not been gussed yet, checks whether or not the letter is in the word
+		# If the letter has not been gussed yet, checks whether or not the letter is in the word
 		else:
 			checked_letter = word_game.check_letter(letter)
 			remaining_guesses = word_game.guesses_left()
 			indices_of_letter_in_word = word_game.get_indices_of_letter_in_word(letter)
 			game_response['letter'] = letter
 
-			# if the checked letter is in the word, will include the indices of the checked letter in the word in the response.
-			# will also check if the player has completed the word and won when they make the guess
+			# Enters this condition if the checked_letter is in the word
 			if checked_letter:
+
+				# If the checked letter is in the word, index/indices of the checked letter in the word are included in the response.
 				game_response['indices'] = indices_of_letter_in_word
 
-				#player has won the game after guessing letter
+				# Checks if player has won the game after guessing a letter
 				if word_game.win():
+
+					# Adds a message and final score to the response
 					game_response['message'] = "You win!"
 					game_response["score"] = word_game.get_score()
+
+					# Saves the game to the database along with the corresponding user_id, word score, and whether or not they won
 					save_game = Score(date=datetime.now(), user_id=int(session['user_id']), word=word_game.get_word(), score=word_game.get_score(), 
 						won=word_game.win())
 					db.session.add(save_game)
 					db.session.commit()
 				
-				#player correctly guessed letter, but has not yet won
+				# If the player has not won, they correctly guesses the letter. A message to inform them they guessed the correct letter is included in the response.
 				else:
 					game_response["message"] = "Great Work! Correct Guess!"
 
-			# if the checked letter is not in the word, will include the number of remaing guesses in the response
-			# will also check if the player has lost the game with the wrong guess, and if so, include the whole word in the response
+			# Enter this condition since the checked_letter was not in the word
 			else:
+				# The number of remaining guesses is included in the response when the letter is guessed incorrectly
 				game_response["remaining_guesses"]=remaining_guesses
 
-				# the player guessed an incorrect letter, and lost the game after exceeding incorrect guesses
+				# Enter this condition if player runs out of remaining guesses, and loses
 				if word_game.lose():
+
+					# Adds a message, score, and entire word to response when player loses
 					game_response["message"] = "Sorry, you have lost the game."
 					game_response["word"] = word_game.get_word()
 					game_response["score"] = word_game.get_score()
+
+					# saves the game to the database
 					save_game = Score(date=datetime.now(), user_id=int(session['user_id']), word=word_game.get_word(), score=word_game.get_score(), 
 						won=word_game.win())					
 					db.session.add(save_game)
 					db.session.commit()
 
-				# the player guessed an incorrect letter, but still has more tries
+				# Enter this condition if player still has remaining guesses, and can continue playing
 				else: 
 					game_response["message"] = "Sorry, Incorrect Guess! {} is not in the word. You have {} chances remaining".format(letter, remaining_guesses)
 
+	# Sends a response to the browser
 	return jsonify(game_response)
 
 
@@ -255,12 +277,13 @@ def check():
 def view_leaderboard():
 	''' renders the template for the leaderboard'''
 
-	#redirects to homepage if user not logged in
+	# Redirects to homepage if user not logged in
 	if "user_id" not in session:
 		return redirect("/")
 
-	# queries the database for sum, user_Id, and username, performing a join on the users and scores table,
-	# grouping by score sum by user_id
+	# Queries the database for a sum of users' scores, user_id, and username by joining the users and scores table on user_id
+	# grouping by scores and user_ID, and ordering by sum in descending
+
 	sql = """SELECT SUM(score), scores.user_id, username 
 			FROM scores JOIN users USING (user_id)
 			GROUP BY scores.user_id, username 
@@ -269,9 +292,11 @@ def view_leaderboard():
 			
 	# executing the query, and fetching all records
 	cursor = db.session.execute(sql)
+
+	# gets a list of the game_leader objects
 	game_leaders = cursor.fetchall()
 
-
+	# Uses Jinja2 variarbles with a list of game_leader objects (game_leaders) and length of this list to use in Jinha2 variables for relative ranking
 	return render_template('leaderboard.html', name=session["name"], leaders=game_leaders, length=len(game_leaders))
 
 
@@ -279,20 +304,25 @@ def view_leaderboard():
 def view_game_history():
 	''' a route that allows a user to view all records of game history'''
 
-	#redirecting to home if not logged in
+	# Redirects user to home if not logged in
 	if "user_id" not in session:
 		return redirect("/")
 
-	# queries the database for the word, score, and whether or not the user won in the 
+	# Queries the database for the word, score, and whether or not the user has won the game baed on user_id, 
+	# sorting results by date in descending order
 	sql = """SELECT word, won, score, date
 			FROM scores 
 			WHERE user_id= :user_id
 			ORDER BY date DESC
 			"""
 
-	# gets the scores for a user using based on the user_id, and fethes all of the scores
+	# gets the user_id from the session
 	user_id = session["user_id"]
+
+	# adds user_id to :user_id varbale insql statement, and executes query
 	cursor = db.session.execute(sql, {"user_id": user_id})
+
+	# gets a list of the game_record objects
 	game_record = cursor.fetchall()
 
 	# getting the sum of the users scores
@@ -301,7 +331,7 @@ def view_game_history():
 		scores.append(game.score)
 	sum_scores = sum(scores)
 
-
+	# Uses Jinja2 variables to render game_record information on template
 	return render_template('history.html', game_history=game_record, name=session["name"], scores_total=sum_scores)
 
 
